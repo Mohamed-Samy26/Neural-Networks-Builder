9237b1b58361b77c7bdf7fdf2493cc509e6d6e9c
Mohamed Samy <midoprom@gmail.com>
2023-11-04 01:17:35 +0200

    Code formatted

--- adaline_main.py
+++ adaline_main.py
@@ -1,5 +1,3 @@
-from re import X
-from tkinter import Y
 import adaline as ad
 import pandas as pd
 import preprocessing as pp
@@ -9,30 +7,33 @@ def main():
     adaline = ad.Adaline(epochs=30, learning_rate=0.0001, bias=0.02)
     main_df = pd.read_excel("Dry_Bean_Dataset.xlsx")
     train, test = pp.train_test_split(main_df, test_size=0.4, random_state=78)
-    
+
     train = pp.imputation(train, inplace=True)
     train = pp.standardize_columns(train, inplace=True)
     train = pp.normalize_columns(train, inplace=True)
-    train = pp.signum_encode(train, class1='BOMBAY', class2='CALI', column='Class')
-    
+    train = pp.signum_encode(train, class1="BOMBAY", class2="CALI", column="Class")
+
     X = train[["Area", "Perimeter"]]
     Y = train["Class"]
-    
+
     print(adaline)
     adaline.train(X, Y)
     print(adaline)
-    
+
     test = pp.imputation(test, inplace=True)
     test = pp.standardize_columns(test, inplace=True)
     test = pp.normalize_columns(test, inplace=True)
-    test = pp.signum_encode(test, class1='BOMBAY', class2='CALI', column='Class')
-    
+    test = pp.signum_encode(test, class1="BOMBAY", class2="CALI", column="Class")
+
     x_test = test[["Area", "Perimeter"]]
     y_test = test["Class"]
-    
+
     print(adaline.accuracy(x_test, y_test))
-    adaline.plot_decision_boundary(x_test, y_test, feature_names=["Area", "Perimeter"], labels=['BOMBAY', 'CALI'])
-    adaline.plot_confusion_matrix(x_test, y_test, labels=['BOMBAY', 'CALI'])
-    
+    adaline.plot_decision_boundary(
+        x_test, y_test, feature_names=["Area", "Perimeter"], labels=["BOMBAY", "CALI"]
+    )
+    adaline.plot_confusion_matrix(x_test, y_test, labels=["BOMBAY", "CALI"])
+
+
 if __name__ == "__main__":
     main()
--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-def normalize_columns(df: pd.DataFrame , columns: list = None, inplace: bool = False):
+
+def normalize_columns(df: pd.DataFrame, columns: list = None, inplace: bool = False):
     """Normalize the values of the given columns of a dataframe.
-    
+
     Parameters
     ----------
     df : pd.DataFrame
@@ -12,30 +13,34 @@ def normalize_columns(df: pd.DataFrame , columns: list = None, inplace: bool = F
         The default is None.
     inplace : bool, optional
         Whether to modify the dataframe in place. The default is False.
-    
+
     Returns
     -------
     pd.DataFrame
         The normalized dataframe.
     """
-    
+
     normalized_df = df
-    
+
     if columns is None:
-        columns = df.select_dtypes(include=['float64', 'int64']).columns
-    
+        columns = df.select_dtypes(include=["float64", "int64"]).columns
+
     if not inplace:
         normalized_df = df.copy()
-        
+
     for column in columns:
-        normalized_df[column] = (df[column] - df[column].min()) / (df[column].max() - df[column].min())
-        
+        normalized_df[column] = (df[column] - df[column].min()) / (
+            df[column].max() - df[column].min()
+        )
+
     return df
 
-def imputation(df: pd.DataFrame, columns: list = None,
-                    inplace: bool = False, method: str = "mean"):
+
+def imputation(
+    df: pd.DataFrame, columns: list = None, inplace: bool = False, method: str = "mean"
+):
     """Fill the NaN values of the given columns of a dataframe.
-    
+
     Parameters
     ----------
     df : pd.DataFrame
@@ -45,27 +50,27 @@ def imputation(df: pd.DataFrame, columns: list = None,
         The default is None.
     inplace : bool, optional
         Whether to modify the dataframe in place. The default is False.
-    
+
     method : str, optional
         The method to use for filling. Can be "mean", "median" or "mode".
         The default is "mean".
-    
+
     Returns
     -------
     pd.DataFrame
         The filled dataframe.
     """
-    
+
     filled_df = df
-    
+
     if columns is None:
-        columns = df.select_dtypes(include=['float64', 'int64']).columns
-    
+        columns = df.select_dtypes(include=["float64", "int64"]).columns
+
     if not inplace:
         filled_df = df.copy()
-        
+
     for column in columns:
-        if method == "mean":        
+        if method == "mean":
             filled_df[column].fillna(df[column].mean(), inplace=True)
         elif method == "median":
             filled_df[column].fillna(df[column].median(), inplace=True)
@@ -73,12 +78,13 @@ def imputation(df: pd.DataFrame, columns: list = None,
             filled_df[column].fillna(df[column].mode(), inplace=True)
         else:
             raise ValueError(f"Invalid method: {method}")
-        
+
     return filled_df
+
 
 def one_hot_encoding(df: pd.DataFrame, columns: list = None, inplace: bool = False):
     """One hot encode the given columns of a dataframe.
-    
+
     Parameters
     ----------
     df : pd.DataFrame
@@ -88,30 +94,33 @@ def one_hot_encoding(df: pd.DataFrame, columns: list = None, inplace: bool = Fal
         The default is None.
     inplace : bool, optional
         Whether to modify the dataframe in place. The default is False.
-    
+
     Returns
     -------
     pd.DataFrame
         The one hot encoded dataframe.
     """
-    
+
     encoded_df = df
-    
+
     if columns is None:
-        columns = df.select_dtypes(include=['object']).columns
-    
+        columns = df.select_dtypes(include=["object"]).columns
+
     if not inplace:
         encoded_df = df.copy()
-        
+
     for column in columns:
-        encoded_df = pd.concat([encoded_df, pd.get_dummies(encoded_df[column], prefix=column)], axis=1)
+        encoded_df = pd.concat(
+            [encoded_df, pd.get_dummies(encoded_df[column], prefix=column)], axis=1
+        )
         encoded_df.drop(column, axis=1, inplace=True)
-        
+
     return encoded_df
+
 
 def standardize_columns(df: pd.DataFrame, columns: list = None, inplace: bool = False):
     """Standardize the values of the given columns of a dataframe.
-    
+
     Parameters
     ----------
     df : pd.DataFrame
@@ -121,35 +130,38 @@ def standardize_columns(df: pd.DataFrame, columns: list = None, inplace: bool = 
         The default is None.
     inplace : bool, optional
         Whether to modify the dataframe in place. The default is False.
-    
+
     Returns
     -------
     pd.DataFrame
         The standardized dataframe.
     """
-    
+
     standardized_df = df
-    
+
     if columns is None:
-        columns = df.select_dtypes(include=['float64', 'int64']).columns
-    
+        columns = df.select_dtypes(include=["float64", "int64"]).columns
+
     if not inplace:
         standardized_df = df.copy()
-        
+
     for column in columns:
         standardized_df[column] = (df[column] - df[column].mean()) / df[column].std()
-        
+
     return standardized_df
 
-def signum_encode(df: pd.DataFrame, column:str, class1:str, class2:str, replace: bool = True):
+
+def signum_encode(
+    df: pd.DataFrame, column: str, class1: str, class2: str, replace: bool = True
+):
     """Signum encode the given columns of a dataframe.
-    
+
     Parameters
     ----------
     df : pd.DataFrame
         The dataframe to signum encode.
     column : str
-        The column to signum encode.    
+        The column to signum encode.
     class1 : str
         The first class to encode.
     class2 : str
@@ -158,69 +170,84 @@ def signum_encode(df: pd.DataFrame, column:str, class1:str, class2:str, replace:
         Whether to modify the dataframe in place. The default is False.
     replace : bool, optional
         Whether to replace the original column. The default is False.
-    
+
     Returns
     -------
     pd.DataFrame
         The signum encoded dataframe.
     """
-    
+
     signum_encoded_df = df.copy()
 
     # filter out the rows that are not class1 or class2
-    signum_encoded_df = signum_encoded_df[(signum_encoded_df[column] == class1) | (signum_encoded_df[column] == class2)] 
-    if not replace:        
+    signum_encoded_df = signum_encoded_df[
+        (signum_encoded_df[column] == class1) | (signum_encoded_df[column] == class2)
+    ]
+    if not replace:
         add_column = column + "_signum"
         signum_encoded_df[add_column] = signum_encoded_df[column]
-        signum_encoded_df.loc[:, add_column] = signum_encoded_df[add_column].apply(lambda x: 1 if x == class1 else -1).astype('float64')        
+        signum_encoded_df.loc[:, add_column] = (
+            signum_encoded_df[add_column]
+            .apply(lambda x: 1 if x == class1 else -1)
+            .astype("float64")
+        )
     else:
-        signum_encoded_df.loc[:, column] = signum_encoded_df[column].apply(lambda x: 1 if x == class1 else -1).astype('float64')
+        signum_encoded_df.loc[:, column] = (
+            signum_encoded_df[column]
+            .apply(lambda x: 1 if x == class1 else -1)
+            .astype("float64")
+        )
     return signum_encoded_df
 
-def label_encode(df: pd.DataFrame, column:str, inplace: bool = False, replace: bool = False):
+
+def label_encode(
+    df: pd.DataFrame, column: str, inplace: bool = False, replace: bool = False
+):
     """Label encode the given columns of a dataframe.
-    
+
     Parameters
     ----------
     df : pd.DataFrame
         The dataframe to label encode.
     column : str
-        The column to label encode.    
+        The column to label encode.
     inplace : bool, optional
         Whether to modify the dataframe in place. The default is False.
     replace : bool, optional
         Whether to replace the original column. The default is False.
-    
+
     Returns
     -------
     pd.DataFrame
         The label encoded dataframe.
     """
-    
+
     label_encoded_df = df
-    
+
     if not inplace:
         label_encoded_df = df.copy()
-        
+
     if not replace:
         add_column = column + "_label"
-        label_encoded_df[add_column] = label_encoded_df[column].astype('category').cat.codes
+        label_encoded_df[add_column] = (
+            label_encoded_df[column].astype("category").cat.codes
+        )
     else:
-        label_encoded_df[column] = label_encoded_df[column].astype('category').cat.codes
-        
+        label_encoded_df[column] = label_encoded_df[column].astype("category").cat.codes
+
     return label_encoded_df
 
 
 def train_test_split(df: pd.DataFrame, test_size: float = 0.4, random_state: int = 78):
     """Split the dataframe into training and testing sets.
-    
+
     Parameters
     ----------
     df : pd.DataFrame
         The dataframe to split.
     test_size : float, optional
         The size of the testing set. The default is 0.2.
-    
+
     Returns
     -------
     pd.DataFrame
@@ -228,9 +255,8 @@ def train_test_split(df: pd.DataFrame, test_size: float = 0.4, random_state: int
     pd.DataFrame
         The testing set.
     """
-    
-    train = df.sample(frac=1 - test_size , random_state=random_state)
-    test = df.drop(train.index)
-    
-    return train, test
 
+    train = df.sample(frac=1 - test_size, random_state=random_state)
+    test = df.drop(train.index)
+
+    return train, test
