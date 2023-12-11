b71af84cac76ff9dc658ff31ef4b142515ae691f
Mohamed Samy <midoprom@gmail.com>
2023-12-11 05:24:20 +0200

    Update activation_functions.py

--- helpers/activation_functions.py
+++ helpers/activation_functions.py
@@ -10,8 +10,8 @@ def sigmoid(x):
 
 
 def tanh(x):
-    return np.tanh(x)
-
+    # return np.tanh(x)
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
 def relu(x):
     return np.maximum(0, x)
